22b2b2f1f2ad627bed5f27b770807c4a32a922cc
Alex Henry <tukkek@gmail.com>
2025-10-15 00:00:00 -0300

    Don't warn if open for less than a minute...
    Mostly as a convenience for accidental opens or reloading.
    Even legitimately it's not like losing a minute of work is grave.

--- code/control/exit.js
+++ code/control/exit.js
@@ -6,6 +6,9 @@ function time(){return Date.now()/1_000}
 
 function close(event){if(time()-saved>MINUTE) event.preventDefault()}
 
-export function ready(){window.addEventListener('beforeunload',close,true)}
-
 export function save(){saved=time()}
+
+export function ready(){
+  save()
+  window.addEventListener('beforeunload',close,true)
+}
